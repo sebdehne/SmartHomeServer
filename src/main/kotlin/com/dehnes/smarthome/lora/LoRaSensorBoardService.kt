6861b0389a157e3e2b314dbdcf065b0e92becf50
Sebastian Dehne <sebastian@dehnes.com>
2021-04-21 22:09:01 +0200

    sensor board firmware update handling

--- src/main/kotlin/com/dehnes/smarthome/lora/LoRaSensorBoardService.kt
+++ src/main/kotlin/com/dehnes/smarthome/lora/LoRaSensorBoardService.kt
@@ -1,11 +1,14 @@
 package com.dehnes.smarthome.lora
 
 import com.dehnes.smarthome.garage_door.toInt
-import com.dehnes.smarthome.utils.PersistenceService
-import com.dehnes.smarthome.utils.readLong32Bits
-import com.dehnes.smarthome.utils.timestampSecondsSince2000
+import com.dehnes.smarthome.lora.LoRaPacketType.*
+import com.dehnes.smarthome.utils.*
 import mu.KotlinLogging
+import java.lang.Integer.min
 import java.time.Clock
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.TimeUnit
+import java.util.zip.CRC32
 
 class LoRaSensorBoardService(
     private val loRaConnection: LoRaConnection,
@@ -16,23 +19,23 @@ class LoRaSensorBoardService(
     private val logger = KotlinLogging.logger { }
     private val sensors = mutableMapOf<Int, SensorState>()
 
+    private var firmwareHolder: FirmwareHolder? = null
+
     init {
         loRaConnection.listeners.add { packet ->
-
             synchronized(this) {
-
                 val currentState = sensors[packet.from] ?: SensorState(
                     packet.from,
+                    0,
+                    false,
                     null
                 )
 
                 val updatedState = when (packet.type) {
-                    LoRaPacketType.REQUEST_PING -> {
-                        onPing(currentState, packet)
-                    }
-                    LoRaPacketType.SENSOR_DATA_REQUEST -> {
-                        onSensorData(currentState, packet)
-                    }
+                    REQUEST_PING -> onPing(currentState, packet)
+                    SENSOR_DATA_REQUEST -> onSensorData(currentState, packet)
+                    SENSOR_FIRMWARE_INFO_REQUEST -> onFirmwareInfoRequest(currentState, packet)
+                    SENSOR_FIRMWARE_DATA_REQUEST -> onFirmwareDataRequest(currentState, packet)
                     else -> null
                 }
 
@@ -46,8 +49,120 @@ class LoRaSensorBoardService(
         }
     }
 
-    private fun firmwareUpdateNeeded(sensorId: Int, currentVersion: Int) =
-        persistenceService["sensor.$sensorId.firmware", "1"]!!.toInt() > currentVersion
+    private fun onFirmwareDataRequest(existingState: SensorState, packet: LoRaInboundPacketDecrypted): SensorState {
+
+        val firmwareDataRequest = FirmwareDataRequest(
+            readInt32Bits(packet.payload, 0),
+            packet.payload[4].toInt(),
+            packet.payload[5].toInt(),
+            clock.timestampSecondsSince2000() - clock.millis(),
+            clock.millis()
+        )
+
+        logger.info { "Handling firmwareDataRequest from ${packet.from} - $firmwareDataRequest" }
+
+        firmwareHolder?.let {
+
+            if (firmwareDataRequest.offset > it.data.size) {
+                error("Requested offset too large")
+            }
+
+            val maxBytesPerResponse = min(firmwareDataRequest.bytesPerResponse, maxPayload - 4)
+            var offset = firmwareDataRequest.offset
+            var messagesSent = 0
+            while (messagesSent < firmwareDataRequest.numberResponses) {
+                val nextChunkSize = min(
+                    maxBytesPerResponse,
+                    it.data.size - offset
+                )
+                if (nextChunkSize <= 0) {
+                    logger.info { "No more data to send" }
+                    break
+                }
+                val data = ByteArray(nextChunkSize + 4)
+
+                // write offset
+                System.arraycopy(
+                    offset.to32Bit(),
+                    0,
+                    data,
+                    0,
+                    4
+                )
+
+                // write firmware bytes
+                System.arraycopy(
+                    it.data,
+                    offset,
+                    data,
+                    4, // after offset
+                    nextChunkSize
+                )
+
+                // send chunk
+                val sync = LinkedBlockingQueue<Boolean>()
+                loRaConnection.send(packet.keyId, packet.from, SENSOR_DATA_RESPONSE, data) { sent ->
+                    sync.offer(sent)
+                }
+
+                val sent = sync.poll(5, TimeUnit.SECONDS)
+
+                if (sent == true) {
+                    messagesSent++
+                    offset += data.size
+                } else {
+                    logger.info { "Could not send firmware chunk, giving up" }
+                    break
+                }
+            }
+
+        } ?: logger.error { "No firmware present" }
+
+        return existingState.copy(lastReceivedMessage = firmwareDataRequest)
+    }
+
+    private fun onFirmwareInfoRequest(existingState: SensorState, packet: LoRaInboundPacketDecrypted): SensorState {
+        logger.info { "Handling firmwareInfoRequest from ${packet.from}" }
+
+        val byteArray = ByteArray(8) { 0 }
+        firmwareHolder?.let {
+
+            // totalLength: 4 bytes
+            System.arraycopy(
+                (it.data.size).to32Bit(),
+                0,
+                byteArray,
+                0,
+                4
+            )
+
+            // crc32: 4 bytes
+            val crc32 = CRC32()
+            crc32.update(it.data)
+            val crc32Value = crc32.value
+            System.arraycopy(
+                crc32Value.to32Bit(),
+                0,
+                byteArray,
+                4,
+                4
+            )
+        }
+
+        loRaConnection.send(packet.keyId, packet.from, SENSOR_FIRMWARE_INFO_RESPONSE, byteArray) {
+            if (!it) {
+                logger.info { "Could not send firmware-info-response" }
+            }
+        }
+
+        return existingState.copy(
+            triggerFirmwareUpdate = false,
+            lastReceivedMessage = FirmwareInfoRequest(
+                clock.timestampSecondsSince2000() - packet.timestampSecondsSince2000,
+                clock.millis()
+            )
+        )
+    }
 
     private fun onSensorData(existingState: SensorState, packet: LoRaInboundPacketDecrypted): SensorState {
         val sensorData = SensorData(
@@ -55,23 +170,24 @@ class LoRaSensorBoardService(
             readLong32Bits(packet.payload, 4),
             readLong32Bits(packet.payload, 8),
             readLong32Bits(packet.payload, 12),
-            packet.payload[16].toInt(),
             clock.timestampSecondsSince2000() - packet.timestampSecondsSince2000,
             clock.millis()
         )
+        val firmwareVersion = packet.payload[16].toInt()
         logger.info { "Handling sensorData from ${packet.from}: $sensorData" }
 
         val responsePayload = ByteArray(2)
-        responsePayload[0] = firmwareUpdateNeeded(packet.from, sensorData.firmwareVersion).toInt().toByte()
+        responsePayload[0] = existingState.triggerFirmwareUpdate.toInt().toByte()
         responsePayload[1] = 0 // timeAdjustmentRequired TODO
 
-        loRaConnection.send(packet.keyId, packet.from, LoRaPacketType.SENSOR_DATA_RESPONSE, responsePayload) {
+        loRaConnection.send(packet.keyId, packet.from, SENSOR_DATA_RESPONSE, responsePayload) {
             if (!it) {
                 logger.info { "Could not send sensor-data response" }
             }
         }
 
         return existingState.copy(
+            firmwareVersion = firmwareVersion,
             lastReceivedMessage = sensorData
         )
     }
@@ -81,18 +197,19 @@ class LoRaSensorBoardService(
         logger.info { "Handling ping from ${packet.from}" }
 
         val ping = Ping(
-            packet.payload[0].toInt(),
             clock.timestampSecondsSince2000() - packet.timestampSecondsSince2000,
             clock.millis()
         )
+        val firmwareVersion = packet.payload[0].toInt()
 
-        loRaConnection.send(packet.keyId, packet.from, LoRaPacketType.RESPONSE_PONG, packet.payload) {
+        loRaConnection.send(packet.keyId, packet.from, RESPONSE_PONG, packet.payload) {
             if (!it) {
                 logger.info { "Could not send pong response" }
             }
         }
 
         return existingState.copy(
+            firmwareVersion = firmwareVersion,
             lastReceivedMessage = ping
         )
     }
@@ -100,7 +217,9 @@ class LoRaSensorBoardService(
 
 data class SensorState(
     val id: Int,
-    val lastReceivedMessage: ReceivedMessage?
+    val firmwareVersion: Int,
+    val triggerFirmwareUpdate: Boolean,
+    val lastReceivedMessage: ReceivedMessage? = null
 )
 
 sealed class ReceivedMessage {
@@ -113,13 +232,29 @@ data class SensorData(
     val humidity: Long,
     val adcBattery: Long,
     val adcLight: Long,
-    val firmwareVersion: Int,
     override val timestampDelta: Long,
     override val receivedAt: Long
 ) : ReceivedMessage()
 
 data class Ping(
-    val firmwareVersion: Int,
     override val timestampDelta: Long,
     override val receivedAt: Long
 ) : ReceivedMessage()
+
+data class FirmwareInfoRequest(
+    override val timestampDelta: Long,
+    override val receivedAt: Long
+) : ReceivedMessage()
+
+data class FirmwareDataRequest(
+    val offset: Int,
+    val bytesPerResponse: Int,
+    val numberResponses: Int,
+    override val timestampDelta: Long,
+    override val receivedAt: Long
+) : ReceivedMessage()
+
+data class FirmwareHolder(
+    val filename: String,
+    val data: ByteArray
+)
