8bfd0ac547508f99d6901b298e8ddd00715222fe
Sebastian Dehne <sebastian@dehnes.com>
2024-05-20 10:51:52 +0200

    upgraded dependencies

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './index.css';
 import App from './Components/App/App';
-import reportWebVitals from './reportWebVitals';
 import {CssBaseline, ThemeProvider} from "@mui/material";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import GarageDoor from "./Components/GarageDoor/GarageDoor";
@@ -49,7 +48,3 @@ root.render(
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
